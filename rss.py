5bffd9c144c3ad2ff0481b268ce0c5f0501bc318
Marcos Fonseca <marco12.fonseca>
2020-01-23 00:30:19 -0300

    o codigo está com um bug, em outro momento vou tentar depurar

--- rss.py
+++ rss.py
@@ -1,29 +1,86 @@
 import json
+import os
 import urllib.request
 import xml.etree.ElementTree as ET
 
-class FeedRSS():
-    def __init__(self):
-        self._filenames = list()
-        self._title = list()
-        self._link = list()
+def downloadXML(*urls):
+    filenames = list()
 
-    def downloadXML(self, *urls):
-        for i in range(len(urls)):
-            urllib.request.urlretrieve(urls[i], f".rss{i}.xml")
-            self._filenames.append(f".rss{i}.xml")
+    if os.path.exists(".database") == False:
+        os.mkdir(".database")
 
-    def xtractData(self):
-        for file in self._filenames:
+    for i in range(len(urls)):
+        urllib.request.urlretrieve(urls[i], f".database/.new{i}.xml")
+        filenames.append(f".database/.new{i}.xml")
+
+    return filenames
+
+class AnalyzeRSS():
+    def _xtractData(self, *files):
+        title = list()
+        link = list()
+
+        for file in files:
             root = ET.parse(file).getroot()
             itemElement = root.findall("channel/item")
 
             for i in itemElement:
-                self._title.append(i[0].text)
-                self._link.append(i[1].text)
+                title.append(i[0].text)
+                link.append(i[1].text)
 
-        return self._title, self._link
+        return title, link
 
+    def _newfiles(self):
+        length = 0
+        files = os.listdir(".database")
+
+        for file in files:
+            if file[:3] == "new":
+                length += 1
+
+        filenames = [f".database/.new{x}.xml" for x in range(length)]
+
+        return self._xtractData(filenames)
+
+    def _oldfiles(self):
+        length = 0
+        files = os.listdir(".database")
+
+        for file in files:
+            if file[:3] == "old":
+                length += 1
+
+        if length > 0:
+            filenames = [f".database/.old{x}.xml" for x in range(length)]
+            return self._xtractData(filenames)
+        else:
+            return False, False
+
+    def _updateFilenames(self):
+        files = os.listdir()
+
+        for file in files:
+            if file[:3] == "old":
+                os.remove(file)
+
+        for file in files:
+            if file[:3] == "new":
+                os.rename(file, "new"+file[:3])
+
+    def getData(self):
+        newTitle, newLink = self._newfiles()
+        oldTitle, oldLink = self._oldfiles()
+
+        if oldTitle != False:
+            for element1 in range(len(newTitle)):
+                for element2 in range(len(oldTitle)):
+                    if newTitle[element1] == oldTitle[element2]:
+                        del(newTitle[element1])
+                        del(newLink[element1])
+
+        self._updateFilenames()
+
+        return newTitle, newLink
 
 class DataBot():
     @staticmethod
