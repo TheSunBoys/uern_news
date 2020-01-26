48490ad5b33066b9c07cd0dd71e7b54f591f15f4
Marcos Fonseca <marco12.fonseca>
2020-01-26 00:48:09 -0300

    removido metodos duplicados na classe AnalyzeRSS

--- rss.py
+++ rss.py
@@ -4,18 +4,17 @@ import urllib.request
 import xml.etree.ElementTree as ET
 
 def downloadXML(*urls):
-    filenames = list()
-
     if os.path.exists(".database") == False:
         os.mkdir(".database")
 
     for i in range(len(urls)):
         urllib.request.urlretrieve(urls[i], f".database/.new{i}.xml")
-        filenames.append(f".database/.new{i}.xml")
 
-    return filenames
 
 class AnalyzeRSS():
+    def __init__(self, directory = ".database"):
+        self._dir = directory
+
     def _xtractData(self, files):
         title = list()
         link = list()
@@ -30,47 +29,37 @@ class AnalyzeRSS():
 
         return title, link
 
-    def _newfiles(self):
+    def _searchFiles(self, name):
         length = 0
-        files = os.listdir(".database")
+        files = os.listdir(self._dir)
 
         for file in files:
-            if file[1:4] == "new":
-                length += 1
-
-        filenames = [f".database/.new{x}.xml" for x in range(length)]
-
-        return self._xtractData(filenames)
-
-    def _oldfiles(self):
-        length = 0
-        files = os.listdir(".database")
-
-        for file in files:
-            if file[1:4] == "old":
+            if file[1:4] == name:
                 length += 1
 
         if length > 0:
-            filenames = [f".database/.old{x}.xml" for x in range(length)]
+            filenames = [f"{self._dir}/.{name}{x}.xml" for x in range(length)]
             return self._xtractData(filenames)
         else:
             return False, False
 
+
     def _updateFilenames(self):
-        files = os.listdir(".database")
+        files = os.listdir(self._dir)
 
         for file in files:
             if file[1:4] == "old":
-                os.remove(".database/"+file)
+                os.remove(f"{self._dir}/"+file)
 
         for file in files:
             if file[1:4] == "new":
-                os.rename(".database/"+file, ".database/.old"+file[4:])
+                os.rename(f"{self._dir}/"+file, f"{self._dir}/.old"+file[4:])
 
     def getData(self):
-        newTitle, newLink = self._newfiles()
-        oldTitle, oldLink = self._oldfiles()
+        newTitle, newLink = self._searchFiles("new")
+        oldTitle, oldLink = self._searchFiles("old")
 
+        # delete duplicates
         if oldTitle != False:
             for element in oldTitle:
                 if element in newTitle:
@@ -79,7 +68,7 @@ class AnalyzeRSS():
             for element in oldLink:
                 if element in newLink:
                     del(newLink[newLink.index(element)])
-                    
+
         self._updateFilenames()
 
         return newTitle, newLink
