b73fa805f245c2bc4d041cdf92ef21f6d136f881
Marcos Fonseca <marco12.fonseca>
2020-02-08 21:38:38 -0300

    rascunho de codigo para gerenciar base de dados

---
+++ modules/data.py
@@ -0,0 +1,51 @@
+import json
+import os
+
+class Database():
+    def __init__(self, directory=".database", filename="database.json"):
+        self._directory = directory
+        self._filename = filename
+        self._database = None
+
+        if os.path.exists(self._directory) == False:
+            os.mkdir(self._directory)
+            self._database = {}
+            self._database["waiting"] = []
+            self._database["history"] = []
+        else:
+            with open(f"{self._directory}/{self._filename}", "r") as file:
+                self._database = json.load(file)
+
+    def _removeDuplicate(self, messages):
+        for elt in self._database["history"]:
+            if elt["msg"] in messages:
+                del(messages[messages.index(elt[msg])])
+
+        for elt in self._database["waiting"]:
+            if elt in messages:
+                del(messages[messages.index(elt)])
+
+        return messages
+
+    def _saveDatabase(self):
+        with open(f"{self._directory}/{self._filename}", "w") as file:
+            json.dump(self._database, file, ensure_ascii=False, indent=2)
+
+    def add(self, messages):
+        messages = self._removeDuplicate(messages)
+
+        if len(messages) > 0:
+            self._database["waiting"] += messages
+
+        self._saveDatabase()
+
+    def getWaitingMessages(self):
+        return self._database["waiting"]
+
+    def removeFromListWait(self):
+        pass
+
+
+if __name__ == "__main__":
+    data = Database()
+    data._history()
