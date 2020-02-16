dab6e2dd1bc52ced2b762844f4be7f4c8a240f6d
Marcos Fonseca <marco12.fonseca>
2020-02-16 19:08:54 -0300

    documentação adicionada a classe Database

--- modules/data.py
+++ modules/data.py
@@ -4,6 +4,15 @@ import json
 import os
 
 class Database():
+    """
+    this class is used to manage pending and sent messages,
+    like a database, it allows:
+
+    :save messages that could not be sent
+    :add messages sent to history
+    :analyze messages before adding to the database and remove duplicates
+    if they exist
+    """
     def __init__(self, directory=".database", filename="database.json"):
         self._directory = directory
         self._filename = filename
@@ -22,6 +31,8 @@ class Database():
             self._database["history"] = []
 
     def _removeDuplicate(self, messages, dict_key):
+        """analyzes the database list and removes elements that are already there"""
+
         tem_list = [messages[x]["title"] for x in range(len(messages))]
 
         for elt in self._database[dict_key]:
@@ -34,10 +45,14 @@ class Database():
         return messages
 
     def _saveDatabase(self):
+        """Update the json file with latest data of dict"""
+
         with open(f"{self._directory}/{self._filename}", "w") as file:
             json.dump(self._database, file, ensure_ascii=False, indent=2)
 
     def add(self, messages):
+        """analyze messages before adding to the database and remove duplicates
+        if they exist"""
         messages = self._removeDuplicate(messages, "history")
         messages = self._removeDuplicate(messages, "waiting")
 
@@ -47,9 +62,13 @@ class Database():
         self._saveDatabase()
 
     def getWaitingMessages(self):
+        """returns pending messages"""
         return copy.deepcopy(self._database["waiting"])
 
     def removeFromWaitList(self, message):
+        """removes from the pending message passed as a parameter
+        and adds it to the history"""
+        
         index_ = self._database["waiting"].index(message)
         date = str(datetime.datetime.now())
 
