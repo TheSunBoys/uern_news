87afcf3d0b40a8f2fa66f353268027a792ab5eaf
Marcos Fonseca <marco12.fonseca>
2020-02-21 22:43:18 -0300

    classe DataBot foi removido do arquivo

--- modules/telegram.py
+++ modules/telegram.py
@@ -18,25 +18,3 @@ class BotTelegram():
 
     def getUpdates(self):
         API = f"https://api.telegram.org/bot{self._token}/getUpdates"
-
-class DataBot():
-    @staticmethod
-    def writeJson(token, chat_id):
-        config = {"token": token, "chatId": chat_id}
-
-        with open(".bot.json", "w") as file:
-            json.dump(config, file, ensure_ascii=False, indent=2)
-
-    @staticmethod
-    def readJson():
-        with open(".bot.json", "r") as file:
-            config = json.load(file)
-
-        return config["token"], config["chatId"]
-
-if __name__ == "__main__":
-    # create .bot.json file
-    TOKEN = "your token here"
-    CHAT_ID = "-your chat_id here"
-
-    DataBot.writeJson(TOKEN, CHAT_ID)
