cc5bfeeb16877eb1f70d2b6531c3a828a2030cae
Marcos Fonseca <marco12.fonseca>
2020-03-09 20:27:08 -0300

    agora a classe está mais tolerante a erros

--- modules/telegram.py
+++ modules/telegram.py
@@ -1,4 +1,3 @@
-import json
 import urllib.parse
 import urllib.request
 
@@ -9,12 +8,25 @@ class BotTelegram():
 
     def sendMessage(self, message):
         message = urllib.parse.quote(message)
-        API = f"https://api.telegram.org/bot{self._token}/sendMessage?chat_id={self._chatId}&text={message}"
+        responses = list()
 
-        response = urllib.request.urlopen(API)
+        for id in self._chatId:
+            API = f"https://api.telegram.org/bot{self._token}/sendMessage?chat_id={id}&text={message}"
+  
+            try:
+                urllib.request.urlopen(API)
+                responses.append(True)
+            except:
+                responses.append(None)
+                continue
+
+        return responses
+            
 
     def sendPhoto(self):
         pass
 
     def getUpdates(self):
         API = f"https://api.telegram.org/bot{self._token}/getUpdates"
+        urllib.request.urlretrieve(urls[i], "getUpdates.json")
+
