e452f441a244815a2de119aba0780a1e2c0cc328
Marcos Fonseca <marco12.fonseca>
2020-07-18 18:26:18 -0300

    pequeno servidor criado para testar funcao downloadxml

---
+++ tests/testDownloadXML.py
@@ -0,0 +1,30 @@
+import multiprocessing
+import os
+import random
+import sys
+import time
+
+sys.path.append('..')
+
+import core
+from createXml import createXmlfile
+
+def startSimpleServer(PORT='8080'):
+    os.system(f'python -m http.server {PORT}')
+
+if __name__ == "__main__":
+    FILENAME = 'testDownload.xml'
+    PORT = f'{random.randint(48620, 49150)}'
+    URLS = [f'http://127.0.0.1:{PORT}/{FILENAME}']
+
+    createXmlfile(FILENAME, 4)
+
+    server = multiprocessing.Process(target=startSimpleServer, args=(PORT,))
+    server.start()
+
+    time.sleep(2)
+    print(core.downloadXML(URLS, directory='.'))
+
+    server.terminate()
+
+    
