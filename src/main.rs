75d14baf1d795ef1ffdf10504d2a642688e03754
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-04-13 18:21:48 -0300

    removido dotenv e feito codigo que baixa os arquivos
    - dotenv foi removido talvez por enquanto, caso não econtrar lib
      superior
    - codigo capaz de baixar os arquivos xml's dos links fornecidos

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-//use dotenv::dotenv;
-//use std::env;
+use std::io;
+use std::fs::File;
+use reqwest;
+
+const URLS: [&str; 2] = [
+   "http://portal.uern.br/blog/category/noticias/feed/",
+   "https://aduern.org.br/category/noticias/feed/"
+];
 
 fn main() {
-    /*
-    dotenv().ok();
-    println!("{}", env::var("nome").unwrap());
-    println!("{}", env::var("senha").unwrap());
-
-    let urls = [
-        "http://portal.uern.br/blog/category/noticias/feed/",
-        "https://aduern.org.br/category/noticias/feed/"
-    ];
-    */
+    for (idx, url) in URLS.into_iter().enumerate() {
+        let mut resp = reqwest::blocking::get(url).unwrap();
+        let mut out = File::create(format!("file{idx}.xml")).unwrap();
+        io::copy(&mut resp, &mut out).expect("failed to copy content");
+    }
 }
