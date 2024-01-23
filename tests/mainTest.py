7c3ec5c3fe36834bfabdaed66ac267cf0eb5886b
Marcos Fonseca <marco12.fonseca@hotmail.com>
2024-01-22 23:28:44 -0300

    implementação antiga deletada

--- tests/mainTest.py
+++
@@ -1,8 +0,0 @@
-from testDatabase import testDatabase
-from testAnalyseRSS import testAnalyseRSS
-from testDownloadXML import testDownloadXml
-
-if __name__ == "__main__":
-    testDatabase()
-    testAnalyseRSS()
-    testDownloadXml()
